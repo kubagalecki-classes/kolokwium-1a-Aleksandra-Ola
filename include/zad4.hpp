81bef1ae09ae0e626d168c344a7c938d004c6cea
AleksandraOla1 <ochodun@gmail.com>
2021-01-14 12:55:19 +0000

    ost

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,4 +3,19 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
-double obliczMake()
+double obliczMake(vector<Tagliatelle>&v)
+{
+  double M;
+  for(auto it=v.rbegin();it!=v.rend();++it)
+  {
+    M=M+i*it->ileMaki();
+  }
+  try
+  {
+    if(M>100)
+    throw((int)1);
+    if(M>50)
+    throw((double)2);
+    return M;
+  }
+}
