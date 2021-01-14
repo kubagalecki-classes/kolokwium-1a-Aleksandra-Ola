954810cc24b61bcf99565be1ed6a6715c2548985
AleksandraOla1 <ochodun@gmail.com>
2021-01-14 11:39:27 +0000

    2

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -22,4 +22,4 @@ class Tagliatelle
   Tagliatelle(double l, double w, double r):L(l), W(w),R(r){};
   Tagliatelle(){L=0.5;W=0.5;R=0.5;};
   double ileMaki(unsigned P) const {return P*L*W*(1-R)*C;};
-}
+};
