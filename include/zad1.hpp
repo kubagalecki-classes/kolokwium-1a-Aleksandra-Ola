6bdb068102859fab59077fcdf44d4ea1ce377c03
AleksandraOla1 <ochodun@gmail.com>
2021-01-14 11:36:57 +0000

    1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -1,4 +1,8 @@
 #include "catch.hpp"
+using namespace std;
+#include<iostream>
+
+
 
 // Zad2
 // tutaj definicja klasy Makaron
@@ -6,3 +10,16 @@
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
+
+class Tagliatelle
+{
+  private:
+  double L;
+  double W;
+  double R;
+  static const double C;
+  public:
+  Tagliatelle(double l, double w, double r):L(l), W(w),R(r){};
+  Tagliatelle(){L=0.5;W=0.5;R=0.5;};
+  double ileMaki(unsigned P) const {return P*L*W*(1-R)*C;};
+}
